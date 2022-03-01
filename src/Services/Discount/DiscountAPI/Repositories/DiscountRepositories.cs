f607ada024be0442c6e4998659c7b979f2731107
febrianian <febrian.evolution@gmail.com>
2022-03-01 14:19:27 +0700

    Add Repository interface and class

---
+++ src/Services/Discount/DiscountAPI/Repositories/DiscountRepositories.cs
@@ -0,0 +1,31 @@
+﻿using DiscountAPI.Models;
+using System.Threading.Tasks;
+
+namespace DiscountAPI.Repositories
+{
+    public class DiscountRepositories : IDiscountRepository
+    {
+
+        public DiscountRepositories()
+        {
+
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            throw new System.NotImplementedException();
+        }
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            throw new System.NotImplementedException();
+        }
+        public async Task<bool> UpdateCreateDiscount(Coupon coupon)
+        {
+            throw new System.NotImplementedException();
+        }
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Discount/DiscountAPI/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using DiscountAPI.Models;
+using System.Threading.Tasks;
+
+namespace DiscountAPI.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateCreateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
